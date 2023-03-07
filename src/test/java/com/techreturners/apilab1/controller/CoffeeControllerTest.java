190d2158152375e6fe0b2dd80d217694cef28e79
Raymond <raymond.yiu@gmail.com>
2023-03-07 23:09:59 +0000

    test pass for CoffeeController GET /coffee

---
+++ src/test/java/com/techreturners/apilab1/controller/CoffeeControllerTest.java
@@ -0,0 +1,32 @@
+package com.techreturners.apilab1.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.
+        MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.
+        MockMvcResultMatchers;
+@AutoConfigureMockMvc
+@SpringBootTest
+class CoffeeControllerTest {
+    @Autowired
+    private MockMvc mockMvcController;
+    @Test
+    void coffeeLover() throws Exception {
+        String expectedContent = "I like coffee!";
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/coffeelover"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedContent));
+    }
+
+    @Test
+    void coffee() {
+    }
+}
